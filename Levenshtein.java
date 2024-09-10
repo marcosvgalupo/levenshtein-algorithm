4d7ca6b60eb881d803d6a11b35773814d86ca085
marcosvgalupo <mv.galupo@hotmail.com>
2024-09-10 14:10:01 -0300

    second commit

--- Levenshtein.java
+++ Levenshtein.java
@@ -1,49 +1,75 @@
 public class Levenshtein {
 
-    public static int min3(int a, int b, int c) {
-        return Math.min(Math.min(a, b), c);
+    public static int max3(int a, int b, int c) {
+        return 
+            Math.max(0,
+                Math.max(
+                        Math.max(a, b), c)
+            );
     }
 
     public static int compute_distance(String s1, String s2){
 
-        int len1 = s1.length();
-        int len2 = s2.length();
+        String formula1[] = s1.split(" ");
+		String formula2[] = s2.split(" ");
 
-        int[][] table = new int[len1 + 1][len2 + 1];
+        if (formula1 == null || formula2 == null)
+			return 0;
 
-        for(int i = 0; i <= len1; i++){
-            for(int j = 0; j <= len2; j++){
+        int len1 = formula1.length;
+        int len2 = formula2.length;
 
-                if(i == 0){
-                    table[0][j] = j;
-                }
-                else if(j == 0){
-                    table[i][0] = i;
-                }
+        if (len1 == 0 || len2 == 0) // strings vazias
+			return 0;
+        
+        if(len1 > len2){
+            String[] aux = formula1;
+            formula1 = formula2;
+            formula2 = aux;
 
-                else{
+            len1 = len2;
+            len2 = formula1.length;
+        }
 
-                    int up = table[i -1][j];
-                    int left = table[i][j - 1];
-                    int diagonal = table[i - 1][j - 1];
+        int max = Integer.MIN_VALUE;
 
-                    if(s1.charAt(i - 1) == s2.charAt(j - 1)){
-                        table[i][j] = diagonal;
-                    } else{
-                        table[i][j] = min3(up + 1, left + 1, diagonal + 1);
-                    }
-                }
+        int linha_anterior[] = new int[len1 + 1];
+        int linha_atual[] = new int[len1 + 1];
+
+        int aux[];
+
+        for(int i = 0; i <= len1; i++)
+            linha_anterior[i] = 0;
+
+        for(int j = 1; j <= len2; j++){
+
+            linha_atual[0] = 0; // primeira coluna
+
+            for(int i = 1; i <= len1; i ++){
+
+                int up = linha_anterior[i];
+                int left = linha_atual[i - 1];
+                int diagonal = linha_anterior[i-1];
+
+                linha_atual[i] = max3(up + 1, left + 1, diagonal + 1);
+
+                if(linha_atual[i] > max) 
+                    max = linha_atual[i];
+
+                aux = linha_anterior;
+                linha_anterior = linha_atual;
+                linha_atual = aux;
             }
         }
 
-        return table[len1][len2];
+        return max;
     }
 
 
     public static void main(String[] args) {
 
-        String s1 = "bad";
-        String s2 = "bed";
+        String s1 = "BAD";
+        String s2 = "BAD";
         int distance = compute_distance(s1, s2);
 
         System.out.println("The distance between \"" + s1 + "\" and \"" + s2 + "\" is: " + distance);
